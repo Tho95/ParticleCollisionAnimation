fe580387d78e9f7f06c744c18eb3582b6fd6d1ba
Thomas Huebscher <hubscherthomas@yahoo.de>
2021-02-15 17:19:54 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+# collsision detection between particles and animation
+
+class Particle:
+
+    def __init__(self,radius,mass,color):
+        self.radius = radius
+        self.mass = mass
+        self.color = color
+
+    def getTop(self):
+        pass
+
+    def getBottom(self):
+        pass
+
+    def getLeft(self):
+        pass
+
+    def getRight(self):
+        pass
+'''
+    def getCenter(self):
+        pass
+'''
